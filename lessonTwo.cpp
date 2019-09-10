22fab06cc2d485657f51b3f6e287ea6fe6a88a93
Ewan <646067@student.cavc.ac.uk>
2019-09-10 10:47:41 +0100

    Add project files.

---
+++ lessonTwo.cpp
@@ -0,0 +1,9 @@
+// lessonTwo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "\n";
+}
